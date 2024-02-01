804888381ee412bdc7c44c9730b713e76ffd85ea
xonack <jenshonack@gmail.com>
2024-02-01 17:18:04 -0500

    fix

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -29,7 +29,7 @@ export const LandingNavbar = () => {
                             fill
                         />
                     </div>
-                    <h1 className={cn("text-2xl font-bold text-white", font.className)}>Vaut</h1>
+                    {/* <h1 className={cn("text-2xl font-bold text-white", font.className)}>Vaut</h1> */}
             </Link>
             <div className="flex items-center gap-x-2">
                 <Link href={isSignedIn ? "/dashboard": "/sign-up"}>
